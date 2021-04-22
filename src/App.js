c6923816749253dcc61f79531810ece7eee406b5
Maisie Klaassen <m.klaassen@gillz.nl>
2021-04-22 17:37:39 +0200

    ai

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [dbInfo, setDbInfo] = useState(null);
+
+  const handler = async(e) => {
+    const data = await window.api.getSetInfo({});
+    data !== undefined && setDbInfo(data);
+  }
+
+  console.log(dbInfo);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => {handler()}}>Test test</button>
+
+        {dbInfo !== null && <h1 style={{color:'#FFF'}}>{dbInfo[0].name}</h1>}
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
